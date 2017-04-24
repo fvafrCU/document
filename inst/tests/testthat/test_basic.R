9167c17a9af3598ec23f68c7578c82d310644ac4
fvafrCU <fvafrcu@arcor.de>
2017-04-24 23:59:15 +0200

    runit and dev_test both pass, coverage is near 0.8

--- inst/tests/testthat/test_basic.R
+++ inst/tests/testthat/test_basic.R
@@ -1,8 +1,12 @@
-require("RUnit")
-require("document")
-test_txt <- function() {
-    options(useFancyQuotes = FALSE)
-    output_directory <- file.path(tempdir(), "document_runit_txt")
+if (interactive()) {
+    devtools::load_all()
+} else {
+    library("document")
+}
+library("testthat")
+context("expected files")
+test_that("foo", {
+    output_directory <- file.path(tempdir(), "document_testthat_txt")
     unlink(output_directory, recursive = TRUE)
     dir.create(output_directory)
     for (file_name in list.files(full.names = TRUE, 
@@ -12,12 +16,16 @@ test_txt <- function() {
                                  )
     ) {
         # create current
+        options(useFancyQuotes = FALSE)
         input <- file.path(system.file('tests', 'files', package = 'document'), 
                            sub("\\.txt", ".R", basename(file_name)))
+        print(file_name)
+        print(input)
         txt_name <- document(input, output_directory = output_directory,
                              check = FALSE, runit = TRUE, clean = FALSE)[["txt_path"]]
         current <- readLines(txt_name)
         reference  <- readLines(file_name)
-        checkTrue(identical(current, reference))
+        expect_equal(current, reference)
     }
-}
+                 }
+)
