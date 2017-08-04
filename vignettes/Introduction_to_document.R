2013005fdf31bb87357f54cd1c2a612ff4a58ac7
fvafrCU <fvafrcu@arcor.de>
2017-08-04 09:45:17 +0200

    add vignette's R code

---
+++ vignettes/Introduction_to_document.R
@@ -0,0 +1,4 @@
+## ---- comment = ""-------------------------------------------------------
+path <- system.file("tests", "files", "minimal.R", package = "document")
+cat(readLines(path), sep = "\n")
+
