9837b395cb7a9f37e0fd28d99cda84475bca0439
fvafrCU <fvafrcu@arcor.de>
2017-08-22 12:40:59 +0200

    lint vignettes' codes as well

--- inst/doc/Introduction_to_document.R
+++ inst/doc/Introduction_to_document.R
@@ -8,29 +8,17 @@ d <- document::document(file_name = path,
                                                      "inst", "tests", "files"),
                         check_package = FALSE)
 file.remove(unlist(d[c("txt_path", "pdf_path")]))
-## res <- document(file_name = system.file("tests", "files", "minimal.R",
-##                                         package = "document"),
-##                 check_package = TRUE)
-## cat(res[["check_result"]][["stdout"]], sep = "\n")
-## cat(res[["check_result"]][["stderr"]], sep = "\n")
 path <- system.file("tests", "files", "simple.R", package = "document")
 cat(readLines(path), sep = "\n")
 d <- document::document(file_name = path, check_package = FALSE)
 cat(readLines(d[["txt_path"]]), sep = "\n")
 # owing to Dason Kurkiewicz <dasonk@gmail.com>, 
 # https://github.com/Dasonk/docstring
-options(help_type = "text")
-
-# A pager that outputs to the console
-console_pager <- function(x, ...){
-    input <- readLines(x)
-    # Had some issues with _ getting displayed
-    # in the output console output which
-    # messed up rendering in the created html vignette
-    # So remove that before outputting.
-    input <- gsub("_", "", input)
-    cat(paste(input, collapse = "\n"), "\n")
-}
-options(pager = console_pager)
+# This is only needed for the vignette, you can skip the setting of the option
+# in regular use.
+pager_function <- function(x, ...) return(cat(paste(readLines(x),
+                                                    collapse = "\n"),
+                                              sep = "\n"))
+options(pager = pager_function)
 path <- system.file("tests", "files", "minimal.R", package = "document")
 document::man(x = path, topic = "foo")
