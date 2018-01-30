b6ca4459c4187f74629b34c801ed394ffcf94445
fvafrCU <fvafrcu@arcor.de>
2018-01-30 19:53:09 +0100

    fix failing test

--- inst/tests/testthat/test_basic.R
+++ inst/tests/testthat/test_basic.R
@@ -50,6 +50,10 @@ test_that("simple", {
                                       "simple.R")
               res <- document(file_name, check_package = TRUE, runit = TRUE)
               current <- checks(res[["check_result"]])
+              expect_true(! any(current[c("errors", "warnings")]))
+              res <- document(file_name, check_package = TRUE, runit = TRUE,
+                              check_as_cran = FALSE)
+              current <- checks(res[["check_result"]])
               expect_true(! any(current))
 }
 )
